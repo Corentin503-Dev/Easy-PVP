bba638e357d2784a883b865f65f37e29f0d0d99a
Corentin503 <82731581+Corentin503-Dev@users.noreply.github.com>
2023-02-26 16:47:03 +0100

    Update Main.php

--- src/Corentin503/Main.php
+++ src/Corentin503/Main.php
@@ -2,8 +2,10 @@
 
 namespace Corentin503;
 
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCreationEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
 
@@ -15,11 +17,20 @@ class Main extends PluginBase implements Listener
     {
         self::setInstance($this);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
         $this->getServer()->getCommandMap()->register("", new KBCommand());
     }
 
     public function creationPlayer(PlayerCreationEvent $event)
     {
         $event->setPlayerClass(PlayerCustom::class);
+    }
+
+    public function onDamage(EntityDamageEvent $event)
+    {
+        if ($event->getEntity() instanceof Player) {
+            $delay = (int)$this->getConfig()->get("delay");
+            $event->setAttackCooldown($delay);
+        }
     }
 }
